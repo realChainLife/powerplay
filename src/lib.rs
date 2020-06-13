b0ed8bb2cbaed6e68d90d09d94d263fe66bff88f
realChainLife <realchainlife@protonmail.com>
2020-06-13 18:02:07 +0300

    add cross-chain fns >> blockdata

--- src/lib.rs
+++ src/lib.rs
@@ -52,15 +52,126 @@ impl HeadData {
 
 /// Block data for this parachain.
 #[derive(Default, Clone, Encode, Decode)]
-pub struct CrossChain {
-	/// State to begin from.
-	pub state: u64,
-	/// Amount to add (overflowing)
-	pub add: u64,
+pub struct BlockData {}
+
+pub trait ExtBlockData {
+	fn merge_sort(&self, arr: Vec<u8>) -> PromiseOrValue<Vec<u8>>;
+	fn merge(
+		&self,
+		#[callback]
+		#encode
+		data0: Vec<u8>,
+		#[callback]
+		#encode
+		data1: Vec<u8>,
+	) -> Vec<u8>;
 }
 
-pub fn hash_state(state: u64) -> [u8; 32] {
-	tiny_keccak::keccak256(state.encode().as_slice())
+pub trait ExtStatusMessage {
+    fn set_status(&mut self, message: String);
+    fn get_status(&self, account_id: String) -> Option<String>;
+}
+
+#[near_bindgen]
+impl BlockData {
+    pub fn deploy_status_message(&self, account_id: String, amount: u64) {
+        Promise::new(account_id)
+            .create_account()
+            .transfer(amount as u128)
+            .add_full_access_key(env::signer_account_pk())
+            .deploy_contract(
+                include_bytes!("../status-message-contract/status_message.wasm").to_vec(),
+            );
+    }
+
+    #[result_serializer(borsh)]
+    pub fn merge_sort(&self, arr: Vec<u8>) -> PromiseOrValue<Vec<u8>> {
+        if arr.len() <= 1 {
+            return PromiseOrValue::Value(arr);
+        }
+        let pivot = arr.len() / 2;
+        let arr0 = arr[..pivot].to_vec();
+        let arr1 = arr[pivot..].to_vec();
+        let prepaid_gas = env::prepaid_gas();
+        let account_id = env::current_account_id();
+
+        ext::merge_sort(arr0, &account_id, 0, prepaid_gas / 4)
+            .and(ext::merge_sort(arr1, &account_id, 0, prepaid_gas / 4))
+            .then(ext::merge(&account_id, 0, prepaid_gas / 4))
+            .into()
+    }
+
+    fn internal_merge(&self, arr0: Vec<u8>, arr1: Vec<u8>) -> Vec<u8> {
+        let mut i = 0usize;
+        let mut j = 0usize;
+        let mut result = vec![];
+        loop {
+            if i == arr0.len() {
+                result.extend(&arr1[j..]);
+                break;
+            }
+            if j == arr1.len() {
+                result.extend(&arr0[i..]);
+                break;
+            }
+            if arr0[i] < arr1[j] {
+                result.push(arr0[i]);
+                i += 1;
+            } else {
+                result.push(arr1[j]);
+                j += 1;
+            }
+        }
+        result
+    }
+
+    /// Used for callbacks only. Merges two sorted arrays into one. Panics if it is not called by
+    /// the contract itself.
+    #[result_serializer(borsh)]
+    pub fn merge(
+        &self,
+        #[callback]
+        #[serializer(borsh)]
+        data0: Vec<u8>,
+        #[callback]
+        #[serializer(borsh)]
+        data1: Vec<u8>,
+    ) -> Vec<u8> {
+        env::log(format!("Received {:?} and {:?}", data0, data1).as_bytes());
+        assert_eq!(env::current_account_id(), env::predecessor_account_id());
+        let result = self.internal_merge(data0, data1);
+        env::log(format!("Merged {:?}", result).as_bytes());
+        result
+    }
+
+    //    /// Alternative implementation of merge that demonstrates usage of callback_vec. Uncomment
+    //    /// to use.
+    //    pub fn merge(&self, #[callback_vec] #[serializer(borsh)] arrs: &mut Vec<Vec<u8>>) -> Vec<u8> {
+    //        assert_eq!(env::current_account_id(), env::predecessor_account_id());
+    //        self.internal_merge(arrs.pop().unwrap(), arrs.pop().unwrap())
+    //    }
+
+    pub fn simple_call(&mut self, account_id: String, message: String) {
+        ext_status_message::set_status(message, &account_id, 0, SINGLE_CALL_GAS);
+    }
+    pub fn complex_call(&mut self, account_id: String, message: String) -> Promise {
+        // 1) call status_message to record a message from the signer.
+        // 2) call status_message to retrieve the message of the signer.
+        // 3) return that message as its own result.
+        // Note, for a contract to simply call another contract (1) is sufficient.
+        ext_status_message::set_status(message, &account_id, 0, SINGLE_CALL_GAS).then(
+            ext_status_message::get_status(
+                env::signer_account_id(),
+                &account_id,
+                0,
+                SINGLE_CALL_GAS,
+            ),
+        )
+    }
+
+    pub fn transfer_money(&mut self, account_id: String, amount: u64) {
+        Promise::new(account_id).transfer(amount as u128);
+    }
 }
 
 /// Start state mismatched with parent header's state hash.
