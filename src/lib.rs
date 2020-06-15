f8e03740865a8a67d055fc84c9d7d790ab9f924e
realChainLife <realchainlife@protonmail.com>
2020-06-15 11:33:18 +0300

    add crosschain fns in main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -68,32 +68,30 @@ pub trait BuildCrossChain {
     fn build(
         &self,
         #[callback]
-        #[serializer(borsh)]
+        #[encode]
         data0: Vec<u8>,
         #[callback]
-        #[serializer(borsh)]
+        #[encode]
         data1: Vec<u8>,
     ) -> CrossChain Vec<u8>;
 }
 
-pub trait ChainStatus {
+pub trait ChainStatusMessage {
 	fn set_status(&mut self, message: String);
 	fn get_status(&self, id: String) -> Option<String>;
 }
 
 impl CrossChain {
-    pub fn deploy_status_message(&self, account_id: String, amount: u64) {
+    pub fn deploy_status_message(&self, id: String, amount: u64) {
         Promise::new(account_id)
             .create_account()
             .transfer(amount as u128)
             .add_full_access_key(env::signer_account_pk())
-            .deploy_contract(
-                include_bytes!("../status-message-contract/status_message.wasm").to_vec(),
-            );
+            .deploy_crosschain();
     }
 
-    #[result_serializer(borsh)]
-    pub fn merge_sort(&self, arr: Vec<u8>) -> PromiseOrValue<Vec<u8>> {
+    #[result_encode]
+    pub fn build_sort(&self, arr: Vec<u8>) -> PromiseOrValue<Vec<u8>> {
         if arr.len() <= 1 {
             return PromiseOrValue::Value(arr);
         }
@@ -103,13 +101,13 @@ impl CrossChain {
         let prepaid_gas = env::prepaid_gas();
         let account_id = env::current_account_id();
 
-        ext::merge_sort(arr0, &account_id, 0, prepaid_gas / 4)
-            .and(ext::merge_sort(arr1, &account_id, 0, prepaid_gas / 4))
-            .then(ext::merge(&account_id, 0, prepaid_gas / 4))
+        ext::build_sort(arr0, &account_id, 0)
+            .and(ext::build_sort(arr1, &account_id, 0))
+            .then(ext::build(&account_id, 0))
             .into()
     }
 
-    fn internal_merge(&self, arr0: Vec<u8>, arr1: Vec<u8>) -> Vec<u8> {
+    fn internal_build(&self, arr0: Vec<u8>, arr1: Vec<u8>) -> Vec<u8> {
         let mut i = 0usize;
         let mut j = 0usize;
         let mut result = vec![];
@@ -133,46 +131,32 @@ impl CrossChain {
         result
     }
 
-    /// Used for callbacks only. Merges two sorted arrays into one. Panics if it is not called by
-    /// the contract itself.
-    #[result_serializer(borsh)]
-    pub fn merge(
+    #[result_encode]
+    pub fn build(
         &self,
         #[callback]
-        #[serializer(borsh)]
+        #[encode]
         data0: Vec<u8>,
         #[callback]
-        #[serializer(borsh)]
+        #[encode]
         data1: Vec<u8>,
     ) -> Vec<u8> {
         env::log(format!("Received {:?} and {:?}", data0, data1).as_bytes());
         assert_eq!(env::current_account_id(), env::predecessor_account_id());
         let result = self.internal_merge(data0, data1);
-        env::log(format!("Merged {:?}", result).as_bytes());
+        env::log(format!("Built {:?}", result).as_bytes());
         result
     }
 
-    //    /// Alternative implementation of merge that demonstrates usage of callback_vec. Uncomment
-    //    /// to use.
-    //    pub fn merge(&self, #[callback_vec] #[serializer(borsh)] arrs: &mut Vec<Vec<u8>>) -> Vec<u8> {
-    //        assert_eq!(env::current_account_id(), env::predecessor_account_id());
-    //        self.internal_merge(arrs.pop().unwrap(), arrs.pop().unwrap())
-    //    }
-
     pub fn simple_call(&mut self, account_id: String, message: String) {
-        ext_status_message::set_status(message, &account_id, 0, SINGLE_CALL_GAS);
+        ext_status_message::set_status(message, &account_id, 0);
     }
     pub fn complex_call(&mut self, account_id: String, message: String) -> Promise {
-        // 1) call status_message to record a message from the signer.
-        // 2) call status_message to retrieve the message of the signer.
-        // 3) return that message as its own result.
-        // Note, for a contract to simply call another contract (1) is sufficient.
-        ext_status_message::set_status(message, &account_id, 0, SINGLE_CALL_GAS).then(
+        ext_status_message::set_status(message, &account_id, 0).then(
             ext_status_message::get_status(
                 env::signer_account_id(),
                 &account_id,
                 0,
-                SINGLE_CALL_GAS,
             ),
         )
     }
